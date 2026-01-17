0376340e4b35c72d40e03e913aac421bba49f0d9
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2026-01-17 22:19:53 +0100

    Update nupkg-release-push.sh

--- nupkg-release-push.sh
+++ nupkg-release-push.sh
@@ -9,6 +9,6 @@ rm -rf src/Serilog.Sinks.YandexCloud/bin/Release/*.nupkg
 dotnet clean "$PROJECT_PATH" -c Release
 dotnet build "$PROJECT_PATH" -c Release --no-incremental
 dotnet pack "$PROJECT_PATH" -c Release --no-build
-dotnet nuget push "src/Serilog.Sinks.YandexCloud/bin/Release/*.nupkg" -s "$SOURCE_URL" --allow-insecure-connections
+dotnet nuget push "src/Serilog.Sinks.YandexCloud/bin/Release/*.nupkg" -s "$SOURCE_URL" --allow-insecure-connections --skip-duplicate
 
 echo "--- Ready ---"
