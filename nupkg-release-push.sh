dd0fd9ebfd57d8d48508d2e4f7615bdc8a7c2985
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2026-01-19 04:45:41 +0100

    Update nupkg-release-push.sh

--- nupkg-release-push.sh
+++ nupkg-release-push.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 PROJECT_PATH="./src/Serilog.Sinks.YandexCloud/Serilog.Sinks.YandexCloud.csproj"
-SOURCE_URL="http://localhost:7665/v3/index.json"
+PACKAGE_SOURCE_URL="http://localhost:7665/v3/index.json"
+
+for i in "$@"; do
+  [[ $next == 1 ]] && psurl=$i && next=0
+  [[ $i == "-psurl" ]] && next=1 && found=1
+done
+if [[ $found == 1 && ! $psurl =~ ^http ]]; then
+  if [[ ! $psurl =~ ^http ]]; then
+    echo "Error: \"psurl\" must be URL starting with http"
+    exit 1
+  fi
+  PACKAGE_SOURCE_URL=$psurl
+fi
 
 echo "--- Build latest NuGet Release ---"
 
@@ -9,6 +21,6 @@ rm -rf src/Serilog.Sinks.YandexCloud/bin/Release/*.nupkg
 dotnet clean "$PROJECT_PATH" -c Release
 dotnet build "$PROJECT_PATH" -c Release --no-incremental
 dotnet pack "$PROJECT_PATH" -c Release --no-build
-dotnet nuget push "src/Serilog.Sinks.YandexCloud/bin/Release/*.nupkg" -s "$SOURCE_URL" --allow-insecure-connections --skip-duplicate
+dotnet nuget push "src/Serilog.Sinks.YandexCloud/bin/Release/*.nupkg" -s "$PACKAGE_SOURCE_URL" --allow-insecure-connections --skip-duplicate
 
 echo "--- Ready ---"
